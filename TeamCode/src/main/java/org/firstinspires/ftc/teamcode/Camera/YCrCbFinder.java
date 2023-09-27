0a2478bb0da9b1c75fe26796b829bc85b0dae15d
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2023-09-26 20:29:49 -0500

    Added Jacobs camera code. waiting on hardware to see if it works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Camera/YCrCbFinder.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.Camera;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Camera.PoleDetectionPipeline;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+
+@Autonomous(name="YCrCbFinder")
+@Disabled
+public class YCrCbFinder extends LinearOpMode {
+
+    private OpenCvCamera camera2;//find webcam statement
+
+    private static final int CAMERA_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
+
+    private static final int CAMERA2_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA2_HEIGHT = 360; // height of wanted camera resolution
+
+    double CrLowerUpdate = 150;
+    double CbLowerUpdate = 120;
+    double CrUpperUpdate = 255;
+    double CbUpperUpdate = 255;
+
+    double lowerruntime = 0;
+    double upperruntime = 0;
+
+    // Pink Range                                      Y      Cr     Cb
+    public static Scalar scalarLowerYCrCb = new Scalar(  0.0, 100, 0);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 250.0, 100);
+
+    @Override
+    public void runOpMode()
+    {
+        // OpenCV lift webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera2 = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 2"), cameraMonitorViewId);
+        //OpenCV PoleDetectionPipeline
+        PoleDetectionPipeline myPipeline;
+        camera2.setPipeline(myPipeline = new PoleDetectionPipeline());
+        // Configuration of PoleDetectionPipeline
+        myPipeline.ConfigurePipeline(0, 0,0,0,  CAMERA2_WIDTH, CAMERA2_HEIGHT);
+        myPipeline.ConfigureScalarLower(scalarLowerYCrCb.val[0],scalarLowerYCrCb.val[1],scalarLowerYCrCb.val[2]);
+        myPipeline.ConfigureScalarUpper(scalarUpperYCrCb.val[0],scalarUpperYCrCb.val[1],scalarUpperYCrCb.val[2]);
+        // Webcam Streaming
+
+        camera2.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera2.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        //webcam.openCameraDeviceAsync(() -> webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT));
+
+
+        // Only if you are using ftcdashboard
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = dashboard.getTelemetry();
+        FtcDashboard.getInstance().startCameraStream(camera2, 10);
+
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            if(myPipeline.error){
+                telemetry.addData("Exception: ", myPipeline.debug);
+            }
+            // Only use this line of the code when you want to find the lower and upper values, using Ftc Dashboard (https://acmerobotics.github.io/ftc-dashboard/gettingstarted)
+            testing(myPipeline);
+
+            // Watch our YouTube Tutorial for the better explanation
+
+            telemetry.addData("RectArea: ", myPipeline.getRectArea());
+            telemetry.update();
+        }
+    }
+    public void testing(PoleDetectionPipeline myPipeline){
+        if(lowerruntime + 0.05 < getRuntime()){
+            CrLowerUpdate += -gamepad1.left_stick_y;
+            CbLowerUpdate += gamepad1.left_stick_x;
+            lowerruntime = getRuntime();
+        }
+        if(upperruntime + 0.05 < getRuntime()){
+            CrUpperUpdate += -gamepad1.right_stick_y;
+            CbUpperUpdate += gamepad1.right_stick_x;
+            upperruntime = getRuntime();
+        }
+
+        CrLowerUpdate = inValues(CrLowerUpdate, 0, 255);
+        CrUpperUpdate = inValues(CrUpperUpdate, 0, 255);
+        CbLowerUpdate = inValues(CbLowerUpdate, 0, 255);
+        CbUpperUpdate = inValues(CbUpperUpdate, 0, 255);
+
+        myPipeline.ConfigureScalarLower(0.0, CrLowerUpdate, CbLowerUpdate);
+        myPipeline.ConfigureScalarUpper(255.0, CrUpperUpdate, CbUpperUpdate);
+
+        telemetry.addData("lowerCr ", (int)CrLowerUpdate);
+        telemetry.addData("lowerCb ", (int)CbLowerUpdate);
+        telemetry.addData("UpperCr ", (int)CrUpperUpdate);
+        telemetry.addData("UpperCb ", (int)CbUpperUpdate);
+    }
+    public Double inValues(double value, double min, double max){
+        if(value < min){ value = min; }
+        if(value > max){ value = max; }
+        return value;
+    }
+}
