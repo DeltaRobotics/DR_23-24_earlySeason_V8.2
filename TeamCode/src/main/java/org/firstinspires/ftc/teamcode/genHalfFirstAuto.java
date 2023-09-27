737457934d07e1dbdd2b04a56a37399d432c5867
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2023-09-26 20:58:33 -0500

    Worked on getting camera code in an auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/genHalfFirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/genHalfFirstAuto.java
@@ -18,6 +18,7 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Camera.PoleDetectionPipeline;
 import org.opencv.core.Scalar;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -30,6 +31,17 @@ import java.util.ArrayList;
 //@Disabled
 public class genHalfFirstAuto extends LinearOpMode
 {
+    private OpenCvCamera camera2;//find webcam statement
+
+    private static final int CAMERA2_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA2_HEIGHT = 360; // height of wanted camera resolution
+
+    // Yellow Range                                      Y      Cr     Cb
+    public static Scalar scalarLowerYCrCb = new Scalar(  0.0, 180, 90);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 170);
+
+    double poleOffset = 0;
+    double poleOffsetPower = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -40,7 +52,41 @@ public class genHalfFirstAuto extends LinearOpMode
 
         robot.resetDriveEncoders();
 
+        // OpenCV lift webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera2 = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 2"), cameraMonitorViewId);
+        //OpenCV PoleDetectionPipeline
+        PoleDetectionPipeline myPipeline;
+        camera2.setPipeline(myPipeline = new PoleDetectionPipeline());
+        // Configuration of PoleDetectionPipeline
+        myPipeline.ConfigurePipeline(0, 0,0,0,  CAMERA2_WIDTH, CAMERA2_HEIGHT);
+        myPipeline.ConfigureScalarLower(scalarLowerYCrCb.val[0],scalarLowerYCrCb.val[1],scalarLowerYCrCb.val[2]);
+        myPipeline.ConfigureScalarUpper(scalarUpperYCrCb.val[0],scalarUpperYCrCb.val[1],scalarUpperYCrCb.val[2]);
+
+        telemetry.addData("Pipeline Ready", 0);
+
+        telemetry.update();
+
+        camera2.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera2.startStreaming(CAMERA2_WIDTH, CAMERA2_HEIGHT, OpenCvCameraRotation.UPSIDE_DOWN);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                telemetry.addData("camera not started", myPipeline.debug);
+            }
+        });
+        FtcDashboard.getInstance().startCameraStream(camera2, 10);
+
         waitForStart();
+
+
+
 
 
 
